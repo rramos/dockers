0edba88bc9f4f9198fc096c9b8654a966a74868b
Rui Ramos <rui.ms.ramos@gmail.com>
2024-12-07 01:20:36 +0000

    Validated pulsar

--- docker-pulsar/producer.py
+++ docker-pulsar/producer.py
@@ -2,10 +2,10 @@
 
 import pulsar
 
-client = pulsar.Client('pulsar://localhost:6650')
-producer = client.create_producer('my-topic')
+client = pulsar.Client("pulsar://localhost:6650")
+producer = client.create_producer("my-topic")
 
 for i in range(10):
-    producer.send(('hello-pulsar-%d' % i).encode('utf-8'))
+    producer.send(("hello-pulsar-%d" % i).encode("utf-8"))
 
 client.close()
