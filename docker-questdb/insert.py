16a648b703c81d63efd4a67ed932f0a0307651d2
Rui Ramos <rui.ms.ramos@gmail.com>
2025-06-10 15:41:48 +0100

    Added docker for questdb

---
+++ docker-questdb/insert.py
@@ -0,0 +1,17 @@
+from questdb.ingress import Sender, TimestampNanos
+
+conf = f"http::addr=172.25.0.2:9000;"
+with Sender.from_conf(conf) as sender:
+    sender.row(
+        "trades",
+        symbols={"symbol": "ETH-USD", "side": "sell"},
+        columns={"price": 2615.54, "amount": 0.00044},
+        at=TimestampNanos.now(),
+    )
+    sender.row(
+        "trades",
+        symbols={"symbol": "BTC-USD", "side": "sell"},
+        columns={"price": 39269.98, "amount": 0.001},
+        at=TimestampNanos.now(),
+    )
+    sender.flush()
